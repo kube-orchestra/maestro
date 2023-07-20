fc6e79fa83e3797cfc5edea0ccadd8ea3cb4b528
Jaime Melis <jmelis@redhat.com>
2023-07-20 21:37:21 +0200

    improve makefile with table generation

--- hack/dynamodb-local-env.sh
+++ hack/dynamodb-local-env.sh
@@ -1,4 +1,4 @@
-export AWS_ACCESS_KEY_ID=dummy
-export AWS_SECRET_ACCESS_KEY=dummy
+export AWS_ACCESS_KEY_ID=x
+export AWS_SECRET_ACCESS_KEY=x
 export PAGER=cat
 alias aws='aws --region us-east-1 --endpoint-url http://localhost:8000'
